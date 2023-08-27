9f7db722d3c00caa877859fafa7b133728eda285
scherbak <scherbak.pavel5@yandex.ru>
2023-08-27 16:06:45 +0400

    Пример работы с Setting

---
+++ src/main/java/com/example/plugin/settings/AppSettingsState.java
@@ -0,0 +1,35 @@
+package com.example.plugin.settings;
+
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.components.State;
+import com.intellij.openapi.components.Storage;
+import com.intellij.util.xmlb.XmlSerializerUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+@State(
+        name = "org.intellij.sdk.settings.AppSettingsState",
+        storages = @Storage("SdkSettingsPlugin.xml")
+)
+public class AppSettingsState implements PersistentStateComponent<AppSettingsState> {
+
+  public String userId = "John Q. Public";
+  public boolean ideaStatus = false;
+
+  public static AppSettingsState getInstance() {
+    return ApplicationManager.getApplication().getService(AppSettingsState.class);
+  }
+
+  @Nullable
+  @Override
+  public AppSettingsState getState() {
+    return this;
+  }
+
+  @Override
+  public void loadState(@NotNull AppSettingsState state) {
+    XmlSerializerUtil.copyBean(state, this);
+  }
+
+}
